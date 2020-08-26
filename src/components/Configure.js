4a7aa0293c3b10ddf49018e580b64126fd15a184
Suman Kumar <suman98kumar@gmail.com>
2020-08-26 23:57:22 +0530

    CRUD Table

--- src/components/Configure.js
+++ src/components/Configure.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CRUDTable from './CRUDTable'
 
-export default class Configure extends React.Component {
+export default class Transaction extends React.Component {
     render() {
         return (
-            <h1>Configure</h1 >
+            <CRUDTable/>
         );
     }
 } 
