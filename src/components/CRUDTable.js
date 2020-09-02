bfb0addebfe053dc2bcf6cc23fe0c580b3f074ad
Suman Kumar <suman98kumar@gmail.com>
2020-09-03 03:53:37 +0530

    Binding data with Material UI table

--- src/components/CRUDTable.js
+++ src/components/CRUDTable.js
@@ -6,53 +6,34 @@ import { Add, Edit, Delete } from '@material-ui/icons'
 import FormDialog from './FormDialog'
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
+  { id: 'Name', label: 'Name' },
+  { id: 'Location', label: 'Location' },
+  { id: 'Description', label: 'Description' },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+// function createData(name, code, population, size) {
+//   const density = population / size;
+//   const X = "suman"
+//   return { name, code, population, size, density, X };
+// }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+// const rows = [
+//   createData('India', 'IN', 1324171354, 3287263),
+//   createData('China', 'CN', 1403500365, 9596961),
+//   createData('Italy', 'IT', 60483973, 301340),
+//   createData('United States', 'US', 327167434, 9833520),
+//   createData('Canada', 'CA', 37602103, 9984670),
+//   createData('Australia', 'AU', 25475400, 7692024),
+//   createData('Germany', 'DE', 83019200, 357578),
+//   createData('Ireland', 'IE', 4857000, 70273),
+//   createData('Mexico', 'MX', 126577691, 1972550),
+//   createData('Japan', 'JP', 126317000, 377973),
+//   createData('France', 'FR', 67022000, 640679),
+//   createData('United Kingdom', 'GB', 67545757, 242495),
+//   createData('Russia', 'RU', 146793744, 17098246),
+//   createData('Nigeria', 'NG', 200962417, 923768),
+//   createData('Brazil', 'BR', 210147125, 8515767),
+// ];
 
 export default class StickyHeadTable extends React.Component {
   constructor(props) {
@@ -102,7 +83,6 @@ export default class StickyHeadTable extends React.Component {
                   {columns.map((column) => (
                     <TableCell
                       key={column.id}
-                      align={column.align}
                     >
                       {column.label}
                     </TableCell>
@@ -110,26 +90,25 @@ export default class StickyHeadTable extends React.Component {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map((row) => {
+                {this.props.rows.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map((row) => {
                   return (
-                    <TableRow hover tabIndex={-1} key={row.code}>
+                    <TableRow hover tabIndex={-1} key={row.id}>
                       <TableCell>
                         <Tooltip title="Edit">
-                          <IconButton onClick={this.handleDialogOpen.bind(this, { mode: 'Edit', id: row.code })}>
+                          <IconButton onClick={this.handleDialogOpen.bind(this, { mode: 'Edit', id: row.id })}>
                             <Edit fontSize="small" />
                           </IconButton>
                         </Tooltip>
                         <Tooltip title="Delete">
-                          <IconButton onClick={this.handleDialogOpen.bind(this, { mode: 'Delete', id: row.code })}>
+                          <IconButton onClick={this.handleDialogOpen.bind(this, { mode: 'Delete', id: row.id })}>
                             <Delete fontSize="small" />
                           </IconButton>
                         </Tooltip>
                       </TableCell>
                       {columns.map((column) => {
-                        const value = row[column.id];
                         return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === 'number' ? column.format(value) : value}
+                          <TableCell>
+                            {row[column.id]}
                           </TableCell>
                         );
                       })}
@@ -140,7 +119,7 @@ export default class StickyHeadTable extends React.Component {
             </Table>
           </TableContainer>
           <TablePagination
-            count={rows.length}
+            count={this.props.rows.length}
             rowsPerPage={this.state.rowsPerPage}
             page={this.state.page}
             onChangePage={this.handleChangePage}
--- src/components/Configure.js
+++ src/components/Configure.js
@@ -2,17 +2,18 @@ import React from 'react'
 import CRUDTable from './CRUDTable'
 import { AppBar, Tabs, Tab, Box } from "@material-ui/core"
 import axios from 'axios'
+import Skeleton from 'react-loading-skeleton'
 
 export default class Configure extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { selectedIndex: 0 }
+        this.state = { selectedIndex: 0, warehouses: null }
     }
 
     componentDidMount() {
         axios.get('/warehouses/').then(resp => {
-            console.log(resp.data)
-        })
+            this.setState({ warehouses: resp.data })
+        })//.catch(e => this.setState({ warehouses: e.response.status }))
     }
 
     handleChange = (event, newValue) => {
@@ -21,25 +22,32 @@ export default class Configure extends React.Component {
         })
     };
 
+    renderBody = () => {
+        switch (this.state.warehouses) {
+            case null:
+                return <Skeleton count={10} height={50} />
+            case 500:
+                return alert(this.state.warehouses)
+            default:
+                return <CRUDTable rows={this.state.warehouses} />
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
                 <h1>Configure</h1>
-                <AppBar position="static" color="default">
-                    <Tabs
-                        value={this.state.selectedIndex}
-                        onChange={this.handleChange}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        variant="fullWidth"
-                    >
-                        <Tab label="Warehouse" />
-                        <Tab label="Items" />
-                        <Tab label="People" />
-                    </Tabs>
-                </AppBar>
-                <Box component="span" m={1}>
-                    <CRUDTable />
+                <Box mb={3}>
+                    <AppBar position="static" color="default">
+                        <Tabs value={this.state.selectedIndex} onChange={this.handleChange} indicatorColor="primary" textColor="primary" variant="fullWidth">
+                            <Tab label="Warehouses" />
+                            <Tab label="Items" />
+                            <Tab label="Contacts" />
+                        </Tabs>
+                    </AppBar>
+                </Box>
+                <Box>
+                    {this.renderBody}
                 </Box>
             </React.Fragment>
         );
