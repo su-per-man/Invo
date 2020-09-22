f6da6a962e6dbb781e80f274a363814b5e981493
Suman Kumar <suman98kumar@gmail.com>
2020-09-22 23:43:19 +0530

    Multivalue unique

--- src/backend/models/Contact.js
+++ src/backend/models/Contact.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose')
 
 let ContactSchema = new mongoose.Schema({
-    FirstName: String,
+    FirstName: {
+        type: String,
+        required: true,
+    },
     LastName: String,
-    Company: String,
-    Location: String,
+    Company: {
+        type: String,
+        required: true
+    },
+    Location: {
+        type: String,
+        required: true
+    },
     Phone: String,
 }, { timestamps: true })
+
+ContactSchema.index({
+    FirstName: 1,
+    Company: 1,
+    Location: 1
+}, { unique: true })
 module.exports = mongoose.model('contacts', ContactSchema)
