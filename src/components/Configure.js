84a56480f1340d4f7b367b9809d1a6360436d2f4
Suman Kumar <suman98kumar@gmail.com>
2020-09-23 10:11:02 +0530

    Error Handling and Snackbar

--- src/components/Configure.js
+++ src/components/Configure.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CRUDTable from './CRUDTable'
-import { AppBar, Tabs, Tab, Box } from "@material-ui/core"
+import { AppBar, Tabs, Tab, Box, Snackbar } from "@material-ui/core"
 import { Alert } from '@material-ui/lab'
 import axios from 'axios'
 import Skeleton from 'react-loading-skeleton'
@@ -15,7 +15,12 @@ export default class Configure extends React.Component {
             { label: "items", value: "item", columns: Configure_Item },
             { label: "contacts", value: "contact", columns: Configure_Contact }
         ]
-        this.state = { selectedTab: this._configurableTabs[0].value, dataVar: null }
+        this.state = {
+            selectedTab: this._configurableTabs[0].value,
+            dataVar: null,
+            showStatus: false,
+            statusMessage: null
+        }
     }
 
     componentDidMount() {
@@ -25,7 +30,7 @@ export default class Configure extends React.Component {
     fetchAll() {
         axios.get('/invo-api/' + this.state.selectedTab).then(resp => {
             this.setState({ dataVar: resp.data })
-        }).catch(e => this.setState({ dataVar: e.response.status }))
+        }).catch(this.handleError)
     }
 
     handleChange = (event, newValue) => {
@@ -36,23 +41,30 @@ export default class Configure extends React.Component {
     handleDeleteDoc = (docID) => {
         axios.post('/invo-api/delete-' + this.state.selectedTab, { id: docID }).then(resp => {
             this.setState({ dataVar: resp.data })
-        }).catch(e => this.setState({ dataVar: e.response.status }))
+        }).catch(this.handleError)
     }
     handleUpdateDoc = (docID, doc) => {
         axios.post('/invo-api/update-' + this.state.selectedTab, { id: docID, data: doc }).then(resp => {
             this.setState({ dataVar: resp.data })
-        }).catch(e => this.setState({ dataVar: e.response.status }))
+        }).catch(this.handleError)
     }
     handleCreateDoc = (doc) => {
         axios.post('/invo-api/create-' + this.state.selectedTab, doc).then(resp => {
             this.setState({ dataVar: resp.data })
-        }).catch(e => this.setState({ dataVar: e.response.status }))
+        }).catch(this.handleError)
+    }
+    handleError = (e) => {
+        let temp = e.request.response
+        if (temp.includes("E11000"))
+            this.setState({ showStatus: true, statusMessage: "Already exists. Duplicate is not allowed" })
+        else if (temp.includes("Proxy Error"))
+            this.setState({ dataVar: "Proxy Error" })
     }
 
     renderBody = (status) => {
         if (status === null)
             return <Skeleton count={5} height={50} />
-        else if (status === 500)
+        else if (status === "Proxy Error")
             return <Box>
                 <Box justifyContent="center" display="flex">
                     <File size={200} mood="sad" color="#83D1FB" />
@@ -85,7 +97,15 @@ export default class Configure extends React.Component {
                 <Box>
                     {this.renderBody(this.state.dataVar)}
                 </Box>
-            </React.Fragment>
+                <Snackbar open={this.state.showStatus} autoHideDuration={6000}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    onClose={() => this.setState({ showStatus: false })}
+                    message={this.state.statusMessage}>
+                </Snackbar>
+            </React.Fragment >
         );
     }
 } 
