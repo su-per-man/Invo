f090c0484885ee14fe39681b0a5ac698b4858e58
Suman Kumar <suman98kumar@gmail.com>
2020-10-02 14:52:58 +0530

    Enhancing Dynamic Form Generation

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -1,15 +1,91 @@
 import React from 'react';
-import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle }
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, Grid, FormControl, Select, MenuItem }
     from '@material-ui/core';
+import Skeleton from 'react-loading-skeleton'
 import { DynamicForm, CRUDModes } from '../SharedConstants'
 
 export default class FormDialog extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            formBody: null,
+            dynamicStates: {}
+        }
+    }
+    componentDidUpdate(prevProps) {
+        if (prevProps !== this.props) {
+            this.setState({
+                formBody: null,
+                dynamicStates: {}
+            }, this.generateDynamicForm)
+        }
+    }
     handleSubmit = (e) => {
         e.preventDefault()
         let fd = new FormData(e.target)
         let obj = {}
         fd.forEach((value, key) => { obj[key] = value });
         this.props.onSave(obj)
+    }
+    handleChange(e, id) {
+        let obj = this.state.dynamicStates
+        obj[id] = e.target.value
+        this.setState({
+            dynamicStates: obj,
+        }, this.generateDynamicForm)
+    }
+    getFormattedDate(dt) {
+        dt = (dt && new Date(dt)) || new Date()
+        let month = dt.getMonth() + 1
+        let day = dt.getDate()
+        return dt.getFullYear() + "-" + (month < 10 ? 0 : '') + month + "-" + (day < 10 ? 0 : '') + day
+    }
+    generateDynamicForm() {
+        let formFields = this.props.formFields
+        let breakPoint = 0
+        let tempObj = []
+        let generatedForm =
+            formFields.map(field => {
+                let savedValue = (this.props.mode === CRUDModes.Update) && this.props.formData[field.id]
+                let rawObj = null
+                switch (field.objectType) {
+                    case DynamicForm.TextField:
+                        rawObj = <TextField name={field.id} label={field.label} type={field.inputType}
+                            defaultValue={savedValue || null}
+                            variant="outlined" margin="dense" autoComplete="off" fullWidth />
+                        break;
+                    case DynamicForm.DateField:
+                        rawObj = <TextField name={field.id} label={field.label} type="date"
+                            defaultValue={this.getFormattedDate(savedValue)} InputLabelProps={{ shrink: true }}
+                            variant="outlined" margin="dense" autoComplete="off" fullWidth />
+                        break;
+                    case DynamicForm.SelectField:
+                        rawObj = <FormControl variant="outlined" size="small" margin="dense" fullWidth>
+                            <Select name={field.id} value={this.state.dynamicStates[field.id] || savedValue || field.defaultValue || -1}
+                                onChange={(e) => this.handleChange(e, field.id)}
+                            >
+                                <MenuItem value="-1" disabled><em>{field.label}</em></MenuItem>
+                                {field.dropdownValues.map((itemVal) => {
+                                    return <MenuItem value={itemVal}>{itemVal}</MenuItem>
+                                })}
+                            </Select>
+                        </FormControl>
+                        break;
+                    default:
+                        return console.log("Error Object Type : " + field.objectType)
+                }
+                tempObj.push(<Grid item xs={12} sm={4}> {rawObj} </Grid>)
+                breakPoint++
+                if (breakPoint % 3 === 0 || breakPoint === formFields.length) {
+                    let temp = <Grid container spacing={3}>{tempObj}</Grid>
+                    tempObj = []
+                    return temp
+                }
+                return null
+            })
+        this.setState({
+            formBody: generatedForm
+        })
     }
     render() {
         return (
@@ -19,24 +95,10 @@ export default class FormDialog extends React.Component {
                     <DialogContent>
                         <form onSubmit={this.handleSubmit.bind(this)}>
                             {
-                                this.props.formFields.map((field) => {
-                                    switch (field.objectType) {
-                                        case DynamicForm.TextField: return <TextField
-                                            margin="dense"
-                                            name={field.id}
-                                            label={field.label}
-                                            defaultValue={this.props.mode === CRUDModes.Update ? this.props.formData[field.id] : null}
-                                            variant="outlined"
-                                            autoComplete="off"
-                                            fullWidth
-                                        />
-                                        default:
-                                            return (console.log('Error in FormDialog'))
-                                    }
-                                })
+                                this.state.formBody || <Skeleton count={5} height={50} />
                             }
                             <DialogActions>
-                                <Button onClick={this.props.onDismiss} color="primary">Cancel</Button>
+                                <Button onClick={this.props.onDismiss} color="primary">Discard</Button>
                                 <Button type="submit" color="primary" variant="contained" disableElevation autoFocus>Save</Button>
                             </DialogActions>
                         </form>
