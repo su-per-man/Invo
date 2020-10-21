638e24d8bf013fa078e12b60922e28af3078a8fe
Suman Kumar <suman98kumar@gmail.com>
2020-10-21 23:35:48 +0530

    Prototyping dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { List, ListItem, ListItemAvatar, ListItemText, Box, Divider, Avatar, Typography } from "@material-ui/core"
+import { green, red } from '@material-ui/core/colors'
+import { CallMade, CallReceived } from '@material-ui/icons'
 
 export default class Dashboard extends React.Component {
     render() {
         return (
-            <h1>Dashboard</h1 >
+            <React.Fragment>
+                <h1>Dashboard</h1 >
+                <List>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar style={{ backgroundColor: green[500] }}> <CallReceived /> </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Suman Kumar" secondary="September 14, 2016" />
+                        <Box alignItems="right">
+                            <Typography variant="h6" color="red">₹ {500}</Typography>
+                        </Box>
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar style={{ backgroundColor: red[500] }}> <CallMade /> </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Suman Kumar" secondary="September 14, 2016" />
+                    </ListItem>
+                </List>
+            </React.Fragment>
         );
     }
 } 
