c65ff9eeddf2ca4ac1feb2c36ffd95414ce0709e
Suman Kumar <suman98kumar@gmail.com>
2020-10-26 09:54:20 +0530

    body render fix

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -30,73 +30,72 @@ export default class Dashboard extends React.Component {
                 <h1>Dashboard</h1 >
                 <List>
                     {
-                        () => {
-                            if (this.state.fetchedRows === [])
-                                return <Skeleton count={5} height={50} />
-                            else {
-                                this.state.fetchedRows.map(t => {
-                                    return (
-                                        <Card>
-                                            <CardActionArea onClick={this.handleClick.bind(this, t._id)}>
-                                                <ListItem alignItems="flex-start">
-                                                    <ListItemAvatar>
-                                                        {t.TransactionType === "Buy" && <Avatar style={{ backgroundColor: green[500] }}> <CallReceived /> </Avatar>}
-                                                        {t.TransactionType === "Sell" && <Avatar style={{ backgroundColor: red[500] }}> <CallMade /> </Avatar>}
-                                                    </ListItemAvatar>
-                                                    <ListItemText primary={
-                                                        <React.Fragment>
-                                                            {t.Vendor}
-                                                            <Typography component="span" variant="overline" color="textPrimary"> - Responsible</Typography>
-                                                        </React.Fragment>} secondary={
-                                                            new Date(t.TransactionDate).toLocaleDateString('en-GB', {
-                                                                year: 'numeric', month: 'short', day: 'numeric'
-                                                            })
-                                                        } />
-                                                    <Box alignItems="right">
-                                                        <Typography variant="h6">₹ {t.CostPerUnit * t.TotalUnits}</Typography>
-                                                    </Box>
-                                                </ListItem>
-                                                <Collapse in={this.state.dyncamicCardExpand[t._id]} timeout="auto" unmountOnExit>
-                                                    <ListItem>
-                                                        <Grid container spacing={2}>
-                                                            <Grid item sm>
-                                                                <Grid container direction="row" alignItems="center" spacing={2} aria-label="Warehouse">
-                                                                    <Grid item><Store /></Grid>
-                                                                    <Grid item>
-                                                                        <Typography variant="subtitle2">{t.Warehouse}</Typography>
-                                                                    </Grid>
+                        (this.state.fetchedRows === [] && <Skeleton count={5} height={50} />)
+                        ||
+                        (
+                            this.state.fetchedRows !== [] &&
+                            this.state.fetchedRows.map(t => {
+                                return (
+                                    <Card>
+                                        <CardActionArea onClick={this.handleClick.bind(this, t._id)}>
+                                            <ListItem alignItems="flex-start">
+                                                <ListItemAvatar>
+                                                    {t.TransactionType === "Buy" && <Avatar style={{ backgroundColor: green[500] }}> <CallReceived /> </Avatar>}
+                                                    {t.TransactionType === "Sell" && <Avatar style={{ backgroundColor: red[500] }}> <CallMade /> </Avatar>}
+                                                </ListItemAvatar>
+                                                <ListItemText primary={
+                                                    <React.Fragment>
+                                                        {t.Vendor}
+                                                        <Typography component="span" variant="overline" color="textPrimary"> - Responsible</Typography>
+                                                    </React.Fragment>} secondary={
+                                                        new Date(t.TransactionDate).toLocaleDateString('en-GB', {
+                                                            year: 'numeric', month: 'short', day: 'numeric'
+                                                        })
+                                                    } />
+                                                <Box alignItems="right">
+                                                    <Typography variant="h6">₹ {t.CostPerUnit * t.TotalUnits}</Typography>
+                                                </Box>
+                                            </ListItem>
+                                            <Collapse in={this.state.dyncamicCardExpand[t._id]} timeout="auto" unmountOnExit>
+                                                <ListItem>
+                                                    <Grid container spacing={2}>
+                                                        <Grid item sm>
+                                                            <Grid container direction="row" alignItems="center" spacing={2} aria-label="Warehouse">
+                                                                <Grid item><Store /></Grid>
+                                                                <Grid item>
+                                                                    <Typography variant="subtitle2">{t.Warehouse}</Typography>
                                                                 </Grid>
-                                                            </Grid>
-                                                            <Grid item sm>
-                                                                <Grid container direction="row" alignItems="center" spacing={2} aria-label="Cost/Unit">
-                                                                    <Grid item><LocalOffer /></Grid>
-                                                                    <Typography variant="subtitle2">₹ {t.CostPerUnit}</Typography>
-                                                                </Grid>
-                                                            </Grid>
-                                                            <Grid item sm>
-                                                                <Grid container direction="row" alignItems="center" spacing={2} aria-label="Total Units">
-                                                                    <Grid item><ShoppingCart /></Grid>
-                                                                    <Typography variant="subtitle2">{t.TotalUnits + " " + t.Unit}</Typography>
-                                                                </Grid>
-                                                            </Grid>
-                                                            <Grid item sm>
-                                                                <Grid container direction="row" alignItems="center" spacing={2} aria-label="Item">
-                                                                    <Grid item><Extension /></Grid>
-                                                                    <Typography variant="subtitle2">{t.Item}</Typography>
-                                                                </Grid>
-                                                            </Grid>
-                                                            <Grid item sm={12}>
-                                                                <ListItemText secondary={t.description} />
                                                             </Grid>
                                                         </Grid>
-                                                    </ListItem>
-                                                </Collapse>
-                                            </CardActionArea>
-                                        </Card>
-                                    )
-                                })
-                            }
-                        }
+                                                        <Grid item sm>
+                                                            <Grid container direction="row" alignItems="center" spacing={2} aria-label="Cost/Unit">
+                                                                <Grid item><LocalOffer /></Grid>
+                                                                <Typography variant="subtitle2">₹ {t.CostPerUnit}</Typography>
+                                                            </Grid>
+                                                        </Grid>
+                                                        <Grid item sm>
+                                                            <Grid container direction="row" alignItems="center" spacing={2} aria-label="Total Units">
+                                                                <Grid item><ShoppingCart /></Grid>
+                                                                <Typography variant="subtitle2">{t.TotalUnits + " " + t.Unit}</Typography>
+                                                            </Grid>
+                                                        </Grid>
+                                                        <Grid item sm>
+                                                            <Grid container direction="row" alignItems="center" spacing={2} aria-label="Item">
+                                                                <Grid item><Extension /></Grid>
+                                                                <Typography variant="subtitle2">{t.Item}</Typography>
+                                                            </Grid>
+                                                        </Grid>
+                                                        <Grid item sm={12}>
+                                                            <ListItemText secondary={t.description} />
+                                                        </Grid>
+                                                    </Grid>
+                                                </ListItem>
+                                            </Collapse>
+                                        </CardActionArea>
+                                    </Card>
+                                )
+                            })
+                        )
                     }
                 </List>
             </React.Fragment >
